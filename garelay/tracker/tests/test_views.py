a0fba3493554a2dacb0fdff18da21332aac33fb7
Simon de Haan <simon@praekeltfoundation.org>
2015-10-20 12:33:13 +0200

    flake8 fix

--- garelay/tracker/tests/test_views.py
+++ garelay/tracker/tests/test_views.py
@@ -4,7 +4,6 @@ from datetime import timedelta
 from django.test import TestCase
 from django.test.client import Client
 from django.core.urlresolvers import reverse
-from django.conf import settings
 
 from ..models import TrackingEvent
 
